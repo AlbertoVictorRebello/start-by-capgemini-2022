eddca556c485922bf9246b1da208a4c8c90964a3
albertoVictorRebello <albertorebello@gmail.com>
2022-04-16 14:33:01 -0300

    coding java exercises

---
+++ Logica de Programacao e Algoritmos III/javaExercises/src/main/java/Ex04.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void execute(Scanner scanner) {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.printf("The sum of %d + %d is %d", a, b, a + b);
+    }
+}
---
+++ Logica de Programacao e Algoritmos III/javaExercises/src/main/java/Ex05.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex04 {
+    static final float COMMISSION_FACTOR = 0.15f;
+    public static void execute(Scanner scanner) {
+
+        System.out.println("Please, inform the seller's name:");
+        String name = scanner.next();
+        System.out.println("Please, inform the seller's salary:");
+        float  salary = scanner.nextFloat();
+        System.out.println("Please, inform the seller's sold value:");
+        float  soldValue = scanner.nextFloat();
+
+           System.out.println();
+        System.out.printf("======================== Monthly Report ========================");
+        System.out.println();
+        System.out.printf("Seller name: %s", name);
+        System.out.printf(" - Salary: %.2f ", salary);
+        System.out.printf(" - Sold value: %.2f\n", soldValue);
+        System.out.printf("Total Salary: %.2f\n", salary + soldValue * COMMISSION_FACTOR );
+    }
+}
